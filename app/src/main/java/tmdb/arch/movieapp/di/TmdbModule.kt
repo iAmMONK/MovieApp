2d192893f50bcb69f173b5b07c78b76dcdce2114
Cristian Lungu <chrizlungu97@gmail.com>
2023-09-02 12:26:01 +0200

    Created viewmodels, setup koin and collecting data from viewmodels

---
+++ app/src/main/java/tmdb/arch/movieapp/di/TmdbModule.kt
@@ -0,0 +1,15 @@
+package tmdb.arch.movieapp.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import tmdb.arch.movieapp.ui.screens.details.MoviesDetailsViewModel
+import tmdb.arch.movieapp.ui.screens.discover.DiscoverMoviesViewModel
+import tmdb.arch.movieapp.ui.screens.saved.SavedMoviesViewModel
+import tmdb.arch.movieapp.ui.screens.search.SearchMoviesViewModel
+
+val viewModels = module {
+    viewModel { DiscoverMoviesViewModel() }
+    viewModel { MoviesDetailsViewModel() }
+    viewModel { SearchMoviesViewModel() }
+    viewModel { SavedMoviesViewModel() }
+}
